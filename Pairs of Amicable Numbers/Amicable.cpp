63f916c3b432446ba6626ccffc595aea951ca4d4
Ferhat Telman <111305718+ferhattelman@users.noreply.github.com>
2022-08-18 18:00:39 +0300

    Add files via upload

---
+++ Pairs of Amicable Numbers/Amicable.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h> 
+#include <string>
+
+using namespace std;
+
+int sumOfDivisions(int num) {
+    int sum = 0;
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+int main() {
+
+    int start, end;
+    cout << "\nPlease,enter low limit: ";
+    cin >> start;
+
+    cout << "\nPlease,enter high limit: ";
+    cin >> end;
+
+    for (int i = start; i <= end; i++) {
+        int sum = sumOfDivisions(i);
+        int another = sumOfDivisions(sum);
+        if (i == another && i > sum) {
+            cout << "Amicable number pair: (" << sum << ", " << another << ")";
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
+
